eb0c5acd62d7e2bd70d099b700c640143d1dd7a5
Matthew Coleman <msc5@princeton.edu>
2023-04-17 22:22:03 -0400

    loss device

--- src/func/loss.py
+++ src/func/loss.py
@@ -2,8 +2,10 @@ import torch
 
 
 def mse(output: torch.Tensor, target: torch.Tensor):
+    target = target.to(output.device)
     return ((output - target)**2).mean()
 
 
 def abse(output: torch.Tensor, target: torch.Tensor):
+    target = target.to(output.device)
     return (output - target).abs().mean()
