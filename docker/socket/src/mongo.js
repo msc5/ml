70badf037ddbb4921835be42034a6980afdbff17
Matthew Coleman <msc5@princeton.edu>
2023-07-14 01:18:41 -0600

    plot configs only databases in compose

--- docker/socket/src/mongo.js
+++ docker/socket/src/mongo.js
@@ -1,7 +1,7 @@
 import { MongoClient } from 'mongodb'
 
-const uri = 'mongodb://username:password@mongo:27017/'
-// const uri = 'mongodb://username:password@localhost'
+// const uri = 'mongodb://username:password@mongo:27017/'
+const uri = 'mongodb://username:password@localhost'
 
 const client = new MongoClient(uri)
 const db = client.db('ml')
